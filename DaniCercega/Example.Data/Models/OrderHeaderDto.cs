b505b9d9e5f04b5b98389bc023fd98e4abec9434
DaniCerega <93987448+DaniCerega@users.noreply.github.com>
2021-11-25 21:07:59 +0200

    Delete DaniCercega directory

--- DaniCercega/Example.Data/Models/OrderHeaderDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Example.Data.Models
-{
-    public class OrderHeaderDto
-    {
-        public int OrderId { get; set; }
-        public string Address { get; set; }
-        public decimal Total { get; set; }
-    }
-}
